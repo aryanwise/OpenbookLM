502c2ebfbae5aec8b13d8ed3337967f5167333a8
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-11-26 22:43:19 +0530

    Script to run the RAG pipeline

--- src/RAG/main.py
+++ src/RAG/main.py
@@ -1,13 +1,27 @@
 from data_loader import DocumentLoader
-from embedding import EmbeddingPipeline
+# from embedding import EmbeddingPipeline
+from vectorstore import FaissVectorStore
+from search import RAGsearch
+
 if __name__ == "__main__":
-    load_data = DocumentLoader(data_dir="../data")
-    print("Total documents loaded", len(load_data.documents))
+    # Load document
+    docs = DocumentLoader(data_dir="../data")
+    # print("Total documents loaded", len(docs.documents))
     # document example
     # TODO: change this to loading document selected by user in frontend 
     # for doc in load_data.documents[:-1]:
         # print(doc.page_content)    
-    emb_pipe = EmbeddingPipeline() # Initialize the embedding pipeline
-    chunks = emb_pipe.chunk_documents(load_data.documents)
-    embeddings = emb_pipe.embed_chunks(chunks)
-    print("[INFO] Example embedding:", embeddings[0] if len(embeddings) > 0 else None)
+
+    # Load vectors
+    store = FaissVectorStore("faiss_store")
+    # TODO: make a function to check if the vector store is already created
+    # if not the run the script below 
+    # store.build_from_documents(docs) 
+    store.load()
+    # print(store.query("What is supervised learning?", top_k=3))
+    rag_search = RAGsearch(llm_model="gemma3:4b")
+    user_query = "What is supervised learning?"
+    print(f"Querying: {user_query}...")
+    
+    summary = rag_search.search_and_summarize(user_query, top_k=3)
+    print("\nSummary Response:\n", summary)
