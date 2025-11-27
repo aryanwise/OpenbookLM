0885dca13c804cc505971672c6bc0887b65fa1da
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-11-27 18:50:06 +0530

    class to load pipeline

--- src/RAG/rag_pipeline.py
+++ src/RAG/rag_pipeline.py
@@ -5,4 +5,21 @@ from search import RAGsearch
 
 class RAGPipeline:
     # Pipeline architecture 
-    ...
+    def __init__(self):
+        self._load_pipeline()
+
+    def _load_pipeline(self):
+        # Load documents
+        DocumentLoader(data_dir="../data")
+
+        # Load vector stores
+        store = FaissVectorStore("faiss_store")
+        store.load()
+        
+        # 
+        rag_search = RAGsearch(llm_model="gemma3:4b")
+        user_query = "What is supervised learning?"
+        print(f"Querying: {user_query}...")
+    
+        summary = rag_search.search_and_summarize(user_query, top_k=3)
+        print("\nSummary Response:\n", summary)
