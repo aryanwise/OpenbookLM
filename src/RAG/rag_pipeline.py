4c4f3afe8af545691fdaa9c5dd7c6b2e7689b70a
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-11-27 18:38:25 +0530

    RAG pipeline chain

---
+++ src/RAG/rag_pipeline.py
@@ -0,0 +1,8 @@
+from data_loader import DocumentLoader
+from vectorstore import FaissVectorStore
+from embedding import EmbeddingPipeline
+from search import RAGsearch
+
+class RAGPipeline:
+    # Pipeline architecture 
+    ...
