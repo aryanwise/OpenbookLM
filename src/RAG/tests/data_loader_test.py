67c21b02fdf6c17b6a212b6dbdaccfea96bd98ec
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-12-02 01:59:32 +0530

    Updates

--- src/RAG/tests/data_loader_test.py
+++ src/RAG/tests/data_loader_test.py
@@ -2,8 +2,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from data_loader import url_extraction
+from data_loader import url_extraction, extract_text, save_txt
 
 
 if __name__ == "__main__":
-    url_extraction(url="https://developer.nvidia.com/discover/artificial-neural-network")
+    # url_extraction(url="https://developer.nvidia.com/discover/artificial-neural-network")
+    doc = extract_text("Aryan Mishra")
+    save_txt(doc=doc)
