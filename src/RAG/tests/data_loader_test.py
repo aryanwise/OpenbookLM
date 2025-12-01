b59c4158fbd20e5944e2c90c7ffaf7072a6b5ecd
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-12-02 02:27:52 +0530

    Updated tests

--- src/RAG/tests/data_loader_test.py
+++ src/RAG/tests/data_loader_test.py
@@ -2,10 +2,22 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from data_loader import url_extraction, extract_text, save_txt
+from data_loader import (
+    DocumentLoader, DocumentManager, ExtractLink, ExtractText
+)
 
 
 if __name__ == "__main__":
-    # url_extraction(url="https://developer.nvidia.com/discover/artificial-neural-network")
-    doc = extract_text("Aryan Mishra")
-    save_txt(doc=doc)
+    # url extraction
+    url_extraction = ExtractLink()
+    url_text = url_extraction.url_extraction("https://developer.nvidia.com/discover/artificial-neural-network")
+    # text extraction 
+    extract_text = ExtractText()
+    text = extract_text.extract_text("Aryan Mishra")
+    
+    # Save the output as .txt 
+    save_text = DocumentManager()
+    save_text.set_storage_location("/Users/aryanmishra/Documents") # Set a temporary storage location
+    save_text.create_load_project("test_project_for_saving") # Create or load a project
+    save_text.save_text_to_project(text_content=str(url_text), file_name="Sample") # Convert url_text to string
+    save_text.save_text_to_project(text_content=text, file_name="Name") 
