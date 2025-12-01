79f538d47dd6528966defcbd65d0afb7b3e8e4d2
Aryan Mishra <105436627+aryanwise@users.noreply.github.com>
2025-12-01 19:54:53 +0530

    Updated tests

--- src/RAG/tests/data_loader_test.py
+++ src/RAG/tests/data_loader_test.py
@@ -2,13 +2,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from data_loader import DocumentLoader, DocumentManager 
+from data_loader import url_extraction
+
 
 if __name__ == "__main__":
-    data_manager = DocumentManager()
-    data_loader = DocumentLoader()
-    try:
-        data_manager.set_storage_location()
-        data_manager.create_project()
-    except Exception as e:
-        print(f"Error: {e}")
+    url_extraction(url="https://developer.nvidia.com/discover/artificial-neural-network")
